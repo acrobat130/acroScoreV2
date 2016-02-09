98f68cc8c0c461011556db656df759988374fd1b
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-02-09 09:43:50 -0800

    connected addScore view to routes, controller, etc. working as expected

---
+++ client/addScore.js
@@ -0,0 +1,9 @@
+angular.module('acroScore.addScore', [
+	// add dependencies here
+
+	])
+
+// if there are factory dependencies, add function it depends on as a function argument
+.controller('AddScoreController', ['$scope', function($scope) {
+	console.log("inside AddScoreController");
+}])
---
+++ client/app.js
@@ -0,0 +1,22 @@
+// module for overall app
+angular.module('acroScore', [
+	// add dependencies here
+	'ui.router',
+	'acroScore.addScore'
+	])
+
+// config adds states that correspond with templates and controllers for each view
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+	function($stateProvider, $urlRouterProvider, $locationProvider) {
+		// default route that goes to index
+		$urlRouterProvider.otherwise('/');
+
+		// controller logic is in js files that correspond to each partial html file
+		// these define the different states/views/controllers
+		$stateProvider
+			.state('addScore', {
+				url: '/',
+				templateUrl: 'addScore.html',
+				controller: 'AddScoreController'
+			})
+	}])
