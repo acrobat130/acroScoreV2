908b1c3fcad6834b3c92d7d7ddaf5b60fe522812
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-02-19 09:29:23 -0800

    added connection to database and connection is working!

--- server/server.js
+++ server/server.js
@@ -1,6 +1,26 @@
-express = require('express');
-// var path = require('path');
+var express = require('express');
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/acroDb'
+
 var app = express();
+
+// initialize connection pool
+// keeps idle connections open for 30 seconds and set limit of 20
+pg.connect(connectionString, function(err, client, done) {
+	if (err) {
+		return console.error('error fetching client from pool', err);
+	}
+	client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+		// call done to release client back to pool
+		done();
+
+		if (err) {
+			return console.error('error running query', err);
+		}
+		console.log(result.rows[0].number);
+		// output: 1
+	});
+});
 
 // this line makes the ../ not seem malicious. doesn't work without this line
 app.use(express.static(__dirname + '/../client'))
@@ -9,7 +29,7 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/../client/index.html');
 });
 
-var server = app.listen(3000, function(){
+app.listen(3000, function(){
 	var host = "127.0.0.1" // server.address.address;
 	var port = 3000 // server.address.port;
 
