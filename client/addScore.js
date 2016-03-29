2949b4c0fe6834647f144ce66e434c81a2f5cd93
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-03-28 22:17:15 -0700

    total score form field autofills, allows user to override input, and validates if the detailed score was added

--- client/addScore.js
+++ client/addScore.js
@@ -7,9 +7,22 @@ angular.module('acroScore.addScore', [
 .controller('AddScoreController', ['$scope', 'getPostFactory', '$location', function($scope, getPostFactory, $location) {
 	console.log("inside AddScoreController");
 
+	// updates total score when other score parameters are edited
 	$scope.updateScore = function() {
 		$scope.totalScore = $scope.artistryScore + $scope.executionScore + $scope.difficultyScore - $scope.penalties;
 	};
+
+	// makes sure that total routine score adds up correctly if user edits the total
+	$scope.validateScore = function() {
+		// if user edits artistry, execution, or difficulty
+		if ($scope.artistryScore !== 0 && $scope.executionScore !== 0 && $scope.difficultyScore !== 0) {
+			// and if total score doesn't equal the sum, alert user
+			if ($scope.totalScore !== $scope.artistryScore + $scope.executionScore + $scope.difficultyScore - $scope.penalties) {
+				alert("The total routine score you entered doesn't match the artistry, execution, difficulty, and penalties you entered. Please double check the scores for accuracy.")
+			}
+		}
+
+	}
 
 	$scope.addScoreToDatabase = function() {
 	console.log("scope.totalScore", $scope.totalScore)
