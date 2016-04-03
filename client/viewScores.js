30206666d4d06eee100e7723b7bd6cbed88c57af
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-04-02 17:59:52 -0700

    changed scope variable name from groupJustPosted to groupQueried

--- client/viewScores.js
+++ client/viewScores.js
@@ -7,19 +7,19 @@ angular.module('acroScore.viewScores', [
 .controller('ViewScoresController', ['$scope', 'getPostFactory', '$location', function($scope, getPostFactory, $location) {
 	console.log("inside ViewScoresController");
 
-	$scope.groupJustPosted = getPostFactory.groupJustPosted;
-	console.log("$scope.groupJustPosted", $scope.groupJustPosted);
+	$scope.groupQueried = getPostFactory.groupJustPosted;
+	console.log("$scope.groupQueried", $scope.groupQueried);
 
-	$scope.thirdAthlete = $scope.groupJustPosted.data[0].athlete3;
-	$scope.fourthAthlete = $scope.groupJustPosted.data[0].athlete4;
+	$scope.thirdAthlete = $scope.groupQueried.data[0].athlete3;
+	$scope.fourthAthlete = $scope.groupQueried.data[0].athlete4;
 
 	$scope.meetTotal = function(meetName) {
 		var meetTotalScore = 0;
-		for (var i = 0; i < $scope.groupJustPosted.data.length; i++) {
+		for (var i = 0; i < $scope.groupQueried.data.length; i++) {
 			// if the meet names match
-			if ($scope.groupJustPosted.data[i].meetName === meetName) {
+			if ($scope.groupQueried.data[i].meetName === meetName) {
 				// add scores together
-				var scoreToBeAdded = Number($scope.groupJustPosted.data[i].total);
+				var scoreToBeAdded = Number($scope.groupQueried.data[i].total);
 				meetTotalScore += scoreToBeAdded;
 			}
 		}
